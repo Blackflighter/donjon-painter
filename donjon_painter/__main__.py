0575318dfe07a5edc9c5f21389fd61b318ad4e3d
Blackflighter <43513164+Blackflighter@users.noreply.github.com>
2018-12-05 01:31:23 +1100

    Implemented interactive mode

--- donjon_painter/__main__.py
+++ donjon_painter/__main__.py
@@ -3,30 +3,52 @@ import argparse
 import time
 from pathlib import Path
 
+
 # Handle arguments
-def getArgs(showHelp = 0):
+def getArgs(showHelp=0):
     parser = argparse.ArgumentParser(
         prog="donjon-painter",
-        description="A tool to create dungeon maps from TSV files (Donjon's Random Dungeon Generator)"
+        description="A tool to create dungeon maps from TSV files \
+        (Donjon's Random Dungeon Generator)"
     )
-    parser.add_argument("MAPFILE", help="specify TSV file to parse", nargs='?', default=0)
-    parser.add_argument("-t", "--tileset", help="select the tileset folder to use (inbuilt theme picker if not used)")
-    parser.add_argument("-m", "--measure", help="get the time it takes to execute the script", action="store_true")
-    parser.add_argument("-o", "--output", help="declare name and/or location of map to save")
-    parser.add_argument("-p", "--pixels", help="specify the size of the squares to scale your image to (default 70)")
-    parser.add_argument("-r", "--randomise", help="rotate the floor tile asset for some variation to the map", action="store_true")
-    parser.add_argument("-s", "--savetiles", help="save any tile assets you created through this script (good for making themes)", action="store_true")
-    
+    parser.add_argument(
+        "MAPFILE", help="specify TSV file to parse", nargs='?')
+    parser.add_argument(
+        "-t", "--tileset",
+        help="select the tileset folder to use (interactive mode if not)")
+    parser.add_argument(
+        "-m", "--measure",
+        help="get the time it takes to execute the script",
+        action="store_true")
+    parser.add_argument(
+        "-o", "--output",
+        help="declare name and/or location of map to save",
+        default=str(Path(Path.home(), 'Map.png')))
+    parser.add_argument(
+        "-p", "--pixels",
+        help="specify the size of your map tile assets (default 70)",
+        default=70)
+    parser.add_argument(
+        "-r", "--randomise",
+        help="rotate the floor tile asset for some variation to the map",
+        action="store_true")
+    parser.add_argument(
+        "-s", "--savetiles",
+        help="generate a theme from a partial one (good for making themes)",
+        action="store_true")
+
     if not showHelp:
         return parser.parse_args()
     else:
         return parser.print_help()
 
+
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    
+
     args = getArgs()
+    '''
     if args.MAPFILE == 0:
         print(getArgs(1))
         print("Map file not specified!",
@@ -34,47 +56,42 @@ def main(args=None):
               "Press Enter to continue.")
         input()
         sys.exit()
-    
-    sys.path.insert(0, str(Path(__file__).parent.resolve()))
-    
-    # Module Files
-    import reader
-    import writer
-    import loader
-    import painter
-    
-    # Execution
-    themeLoc    = reader.getTheme(args.tileset)
-    imagePath   = reader.createImageDirectory(themeLoc)
-    mapArray    = reader.createMapArray(args.MAPFILE)
-    saveDir     = writer.createSaveDirectory(args.output)
-    
-    mapRes = loader.createImagePath(imagePath)
-    
-    if loader.savetiles:
-        print("Writing new tiles to folder...")
-    mapRes = loader.generateRes(args.savetiles, mapRes, imagePath)
+    '''
 
-    if loader.resourcesPresent(mapRes) and mapArray != []:
-        start = time.time()
-        
-        print("Generating rows...")
-        mapRes  = painter.normaliseImages(mapRes, args.pixels)
-        mapRes  = painter.writeRows(mapArray, mapRes, args)
-        print("Merging rows...")
-        mapRes  = painter.mergeRows(mapRes)
-        
-        print("Saving image...")
-        writer.saveMap(mapRes, saveDir)
-        
-        end = time.time()
-        
-        if args.measure:
-            print("Done in", end - start, "seconds.")
-        else:
-            print("Done!")
+    sys.path.insert(0, str(Path(__file__).parent.resolve()))
+
+    # Module Files
+    import menu
+    import themes
+    import imgmap
+
+    # Execution Redux (Interactive Mode vs. Single Command Mode)
+    if args.MAPFILE is None:
+        menu.mainmenu(args)
     else:
-        print("Map generation failed! Insufficient assets.")
-    
+        if args.savetiles:
+            start = time.time()
+            if themes.writeTheme(args.tileset) is False:
+                print("Theme cannot be saved - insufficient resources.")
+            else:
+                if args.measure:
+                    end = time.time()
+                    print("Theme generation done in", end - start, "seconds.")
+        if args.tileset is None:
+            themes.printThemes()
+            args.tileset = themes.selTheme(args.tileset)
+        if args.measure:
+            start = time.time()
+        if imgmap.writeMap(args) is False:
+            print("Map cannot be saved - insufficient resources.")
+        else:
+            if args.measure:
+                end = time.time()
+                print("Map generation done in", end - start, "seconds.")
+            print("Finished!")
+
+
 if __name__ == "__main__":
+    main()
+else:
     main()
