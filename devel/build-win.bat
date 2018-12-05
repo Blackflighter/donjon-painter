9b9f995e288f304223fdf3897822bd949bed677f
Blackflighter <43513164+Blackflighter@users.noreply.github.com>
2018-12-05 13:07:27 +1100

    Script for building on Windows.

---
+++ devel/build-win.bat
@@ -0,0 +1,1 @@
+pyinstaller --name="donjon-painter" --paths="../donjon_painter" --add-data="../donjon_painter/themes;themes" --distpath="../dist" --workpath="../build" --onefile ..\donjon_painter\painter.py
